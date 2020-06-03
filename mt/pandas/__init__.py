351b36170bf345fbe8ce0927d52ea1882b540554
Minh-Tri Pham <pmtri80@gmail.com>
2020-06-03 14:11:43 +0100

    v0.1.3: json accessor and ndarray accessor

--- mt/pandas/__init__.py
+++ mt/pandas/__init__.py
@@ -1,1 +1,3 @@
 from .version import VERSION as __version__
+from .custom_accessors import JsonAccessor, NdarrayAccessor
+
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author=["Minh-Tri Pham"],
     packages=find_packages() + find_namespace_packages(include=['mt.*']),
     install_requires=[
-        'pandas>=0.23.0',  # for dataframes
+        'pandas>=0.23.0',  # for dataframes, and we need custom dtypes
         'dask[dataframe]',  # for reading chunks of CSV files in parallel
         'basemt>=0.3.3',  # Minh-Tri's base modules for logging and multi-threading
     ],
