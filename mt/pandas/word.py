0224f856964b4793d0427329c5c855eb5ea48ac3
Minh-Tri Pham <pmtri80@gmail.com>
2020-06-20 10:37:15 +0100

    v0.1.5: now with n-grams

--- mt/pandas/word.py
+++ mt/pandas/word.py
@@ -5,7 +5,7 @@ import pandas.api.extensions as _pae
 
 
 @_pae.register_series_accessor("ndarray")
-class NdarrayAccessor:
+class WordAccessor:
     '''Accessor for word fields.'''
 
     def __init__(self, pandas_obj):
@@ -20,3 +20,33 @@ class NdarrayAccessor:
     def vietnamese(self):
         '''Returns which item is like a Vietnamese word'''
         return self._obj.str.match("[a-zA-ZàáảãạâầấẩẫậăằắẳẵặđèéẻẽẹêềếểễệìíỉĩịòóỏõọôồốổỗộơờớởỡợùúủũụưừứửữựỳýỷỹỵÀÁẢÃẠÂẦẤẨẪẬĂẰẮẲẴẶĐÈÉẺẼẸÊỀẾỂỄỆÌÍỈĨỊÒÓỎÕỌÔỒỐỔỖỘƠỜỚỞỠỢÙÚỦŨỤƯỪỨỬỮỰỲÝỶỸỴ]+$")
+
+    @property
+    def ngram(self, n):
+        '''Returns a list letter n-grams for each word.
+
+        Parameters
+        ----------
+        n : int
+            number n specifying the letter n-gram
+        
+        Returns
+        -------
+        pandas.Series
+            each element of the returning series is a list of n-grams of the corresponding element in the input series
+        
+        Notes
+        -----
+        You can use pandas' explode() function to process further.
+        '''
+        return self._obj.apply(lambda x: [x[i:i+2] for i in range(len(x)-1)])
+
+    @property
+    def bigram(self):
+        '''Returns a list of letter bigrams for each word. See `ngram()`.'''
+        return self.ngram(2)
+
+    @property
+    def trigram(self):
+        '''Returns a list of letter trigrams for each word. See `ngram()`.'''
+        return self.ngram(3)
